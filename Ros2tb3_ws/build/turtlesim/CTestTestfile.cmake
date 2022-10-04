90e56b0e03db8aa70420acfd8e1364df053b4cd3
RASPODE <RASOPDE.IEEE@gmail.com>
2022-10-04 17:41:29 -0300

    deleted:    Ros2tb3_ws/build/.built_by

--- Ros2tb3_ws/build/turtlesim/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/henrique/Ros2tb3_ws/src/ros_tutorials/turtlesim
-# Build directory: /home/henrique/Ros2tb3_ws/build/turtlesim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("turtlesim__py")
